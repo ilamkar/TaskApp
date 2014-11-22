df4b7a5df5e2c145f27c5c2d0803dc0d8c5c6203
ilamkar <illum_dae@yahoo.com>
2014-11-21 20:25:51 -0600

    completed functionality of list.php

--- pages/list.php
+++ pages/list.php
@@ -1,2 +1,35 @@
 <?php 
-echo  $_GET['id'];
+$list_id  =  $_GET['id'];
+
+// Instianate Database object
+$database  = new Database();
+
+//Query
+$database->query('SELECT * FROM list WHERE id = :id');
+$database->bind(':id',$list_id);
+$row = $database->single();
+
+echo '<h1>'.$row['list_name'].'</h1>';
+echo '<p>'.$row['list_body'].'</p>';
+echo '<a href = "?page= edit_list&id ='.$row['id'].'">Edit List</a> |';
+echo '<a href ="?page=delete_list&id='.$row['id'].'">Delete List</a>';
+
+//Instiance Database object
+$database = new Database();
+
+//Query
+$database->query('SELECT * FROM tasks WHERE list_id =:list_id AND is_complete = :is_complete');
+$databse->bind(':list_id',$list_id);
+$database->bind(':is_complete',0);
+$rows = $database->resultset();
+
+echo '<h3>Tasks</h3>';
+if($rows){
+	echo '<ul class = "items">';
+foreach($rows as $task){
+	echo '<li><a href = "page=task&id='.$task['id'].'">'.$task['task_name'].'</a></li>';
+}
+	echo '</ul>';
+}	else{
+	echo 'No task for this list - <a href="index.php?page=new_task&listid='.$_GET['id'].'">Create one now</a>';
+}
