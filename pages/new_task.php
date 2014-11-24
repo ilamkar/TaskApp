62847fb544e0f137fed438fbcb84b9c39736f7ac
ilamkar <illum_dae@yahoo.com>
2014-11-24 17:40:31 -0600

    added form for new task
    The user will be able to input the task

--- pages/new_task.php
+++ pages/new_task.php
@@ -19,3 +19,37 @@
 		}
 	}
 ?>
+
+<?php
+	$database=new Database();
+
+	//Get logged in user
+	$list_user = $_SESSION['username'];
+
+	//Query
+	$database->query('SELECT * FROM list WHERE list_user=:list_user');
+	$database->bind(':list_user',$list_user);
+	$rows=$database->resultset();
+
+?>
+<h1>Add a Task</h1>
+<form action="<php $_SERVER['PHP_SELF']; ?>" method="post">
+	<label>Task Name</label>
+	<input type="text" name="task_name" /><br/>
+	<?php  if($_GET('list_id')): ?>
+		<input type="hidden" name="list_id" value="<?php echo $_GET['listid'];?>"/>
+	<?php else :?>
+	<label>List</label>
+	<select name="list_id">
+		<option value="0">--Select List--</option>
+		<?php foreach($rows as $list) :?>
+			<option value="<?php echo $list['id'];?>"><?php echo $list['list_name'];?></option>
+	</select>
+		<?php endif;?>
+	<br/>
+	<label>Task Body</label>
+	<textarea rows="5" cols="50" name="task_body"></textarea><br/>
+	<label>Due Date</label>
+	<input type="date" name="due_date"/><br/>
+	<input type="submit" value="Create" name="task_submit"/>
+</form>
