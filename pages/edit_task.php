5172aee9298d13296a514a10afa77128dc85f016
ilamkar <illum_dae@yahoo.com>
2014-11-24 17:41:57 -0600

    edit_task.php
    Added all functionality for form and user can edit task

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -43,14 +43,14 @@
 	<label>Task Name</label><br/>
 	<input type = "text" name = "task_name" value="<?php echo $row['task_name']; ?>" /><br/>
 	<label>Task Body</label><br/>
-	<textarea rows="5" cols ="50" name="task_body"><?php echo $row['task_body']; ?></textarea>
+	<textarea rows="5" cols ="50" name="task_body"><?php echo $row['task_body']; ?></textarea><br/>
 	<label>Due Date</label><br/>
 	<input type="date" name="due_date" value="<?php echo date($row['due_date']); ?>"/><br/>
 	<label>List</label><br/>
 	<select name="list_id">
 		<option value ="0">--Select List--</option>
-		<?php foreach ($rows as $list): ?>
-			<option value="<?php echo $list['id']?>" <?php if($list['id']==$task_id){echo 'selected';} ?>></option>
+		<?php foreach($rows as $list): ?>
+			<option value="<?php echo $list['id'];?>" <?php if($list['id']==$task_id){echo 'selected';} ?>><?php echo $list['list_name'];?></option>
 		<?php endforeach;?>
 	</select>
 	<br/>
