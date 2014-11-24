09d48e1a83e5b6822011ea8c2e0f851c13ba719b
ilamkar <illum_dae@yahoo.com>
2014-11-24 10:37:25 -0600

    Make changes in task
    the user would be able to edit the task from this page

---
+++ pages/edit_task.php
@@ -0,0 +1,61 @@
+<?php
+	if($_POST['submit']){
+		$task_id = $_GET['id'];
+		$task_name = $_POST['task_name'];
+		$task_body = $_POST['task_body'];
+		$due_date = $_POST['due_date'];
+		$list_id = $_POST['list_id'];
+		$is_complete = $_POST['is_complete'];
+
+		$database = new Database();
+
+		$database->query('UPDATE tasks SET task_name=:task_name,task_body=:task_body,due_date=:due_date,list_id=:list_id,is_complete=:is_complete');
+		$database->bind(':task_name',$task_name);
+		$database->bind(':task_body',$task_body);
+		$database->bind(':due_date',$due_date);
+		$database->bind(':list_id',$list_id);
+		$database->bind(':is_complete',$is_complete);
+		$database->execute();
+
+		if($database->rowCount()){
+			echo '<p>Your Task has been updated</p>';
+		}
+	}
+?>
+<?php
+	$database = new Database();
+
+	$database->query('SELECT * FROM list');
+	$rows = $database->resultset();
+?>
+
+<?php
+	$task_id = $_GET['id'];
+	$database = new Database();
+  $database->query('SELECT * FROM tasks WHERE id= :id');
+  $database->bind(':id',$task_id);
+  $row = $database->single();
+
+?>
+
+<h1>Edit Task</h1>
+<form action = "<?php $_SERVER['PHP_SELF']; ?>" method="post">
+	<label>Task Name</label><br/>
+	<input type = "text" name = "task_name" value="<?php echo $row['task_name']; ?>" /><br/>
+	<label>Task Body</label><br/>
+	<textarea rows="5" cols ="50" name="task_body"><?php echo $row['task_body']; ?></textarea>
+	<label>Due Date</label><br/>
+	<input type="date" name="due_date" value="<?php echo date($row['due_date']); ?>"/><br/>
+	<label>List</label><br/>
+	<select name="list_id">
+		<option value ="0">--Select List--</option>
+		<?php foreach ($rows as $list): ?>
+			<option value="<?php echo $list['id']?>" <?php if($list['id']==$task_id){echo 'selected';} ?>></option>
+		<?php endforeach;?>
+	</select>
+	<br/>
+	Mark if Completed <input type="checkbox" name="is_complete" value="1">
+
+	<br/>
+	<input type="submit" value="Update" name="submit"/>
+</form>
