bb4f50c732e045b7390e64d6b74bbe106e49aa09
ilamkar <illum_dae@yahoo.com>
2014-11-24 19:14:51 -0600

    change

--- pages/new_task.php
+++ pages/new_task.php
@@ -33,21 +33,22 @@
 
 ?>
 <h1>Add a Task</h1>
-<form action="<php $_SERVER['PHP_SELF']; ?>" method="post">
+<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
 	<label>Task Name</label>
 	<input type="text" name="task_name" /><br/>
-	<?php  if($_GET('list_id')): ?>
+	<?php if($_GET['listid']): ?>
 		<input type="hidden" name="list_id" value="<?php echo $_GET['listid'];?>"/>
-	<?php else :?>
+	<?php else:?>
 	<label>List</label>
 	<select name="list_id">
 		<option value="0">--Select List--</option>
 		<?php foreach($rows as $list) :?>
 			<option value="<?php echo $list['id'];?>"><?php echo $list['list_name'];?></option>
+		<?php endforeach; ?>
 	</select>
 		<?php endif;?>
 	<br/>
-	<label>Task Body</label>
+	<label>Task Body</label><br/>
 	<textarea rows="5" cols="50" name="task_body"></textarea><br/>
 	<label>Due Date</label>
 	<input type="date" name="due_date"/><br/>
